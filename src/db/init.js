b2c63fb5bbd3a891e35293b94d90f416663d7cec
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-05-14 12:09:36 -0400

    integreated client and server together

--- src/db/init.js
+++ src/db/init.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 mongoose.set('strictQuery', true);
 require('../models/expert');
 
 const connect = async () => {
   try {
+   
     await mongoose.connect(process.env.MONGO_URL);
     console.log('connected');
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 // connecting to database
 const { connect } = require('./db/init');
@@ -11,6 +12,8 @@ app.use(cors({ origin: '*' }));
 //for request body data
 app.use(express.json());
 
+app.use(express.static(path.join(process.cwd(),'client')));
+
 app.get('/', (req, res) => {
   res.send(
     '<div style="width:200px; margin: auto auto;"><img width="100%"  src="https://media.tenor.com/2jd3xi2WVt0AAAAC/recurring-settings.gif"></div><div style="width:220px; margin: 0 auto;"><h2>Server is Running...</h2></div>'
@@ -18,6 +21,20 @@ app.get('/', (req, res) => {
 });
 //routes
 app.use('/api/expert', require('./routes/expert.route'));
+app.use('/api',function(req,res,next){
+  const error = new Error("Unsupported API");
+  error.status =404;
+  next(error);
+});
+app.use('/api',function(error,req,res,next){
+  res.status(error.status || 500);
+  res.send({
+    message:error.message
+  });
+});
+app.use(function(req,res,next){
+  res.sendFile(path.join(process.cwd(),'client','index.html'));
+})
 const PORT = process.env.PORT || 5001;
 connect()
   .then(() => {
